df7f4bc12f5db88eecc6bcf63907bb35013491b4
suyeonsu <agew0423@daum.net>
2022-07-23 21:40:41 +0900

    Add MainImage and GridCards

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,35 +1,63 @@
-import React, { useEffect } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import Auth from '../../../hoc/auth';
+import { Row } from "antd";
+import React, { useEffect, useState } from "react";
+import { API_KEY, API_URL, IMAGE_BASE_URL } from "../../Config";
+import GridCards from "../commons/GridCards";
+import MainImage from "./Sections/MainImage";
 
 function LandingPage() {
 
-    const navigate = useNavigate()
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
 
     useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => { console.log(response) })
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+                console.log(response.results)
+                setMovies([...response.results])
+                setMainMovieImage(response.results[0])
+        })
     }, [])
 
-    const onClickHandler = () => {
-        axios.get('/api/users/logout')
-            .then(response => {
-                if(response.data.success) {
-                    navigate('/login')
-                } else {
-                    alert('Failed to logout')
-                    navigate('/login')
-                }
-            })
-    }
-
     return (
-        <div style={{ display:'flex', justifyContent:'center', alignItems:'center', width:'100%', height:'100vh' }}>
-            <h2>시작 페이지</h2>
-            <button onClick={onClickHandler}>Logout</button>
+        <div style={{ width: '100%', margin: '0' }}>
+            {/* Main Image */}
+            {MainMovieImage &&
+                <MainImage 
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+            }
+
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+                <h2>Movies by latest</h2>
+                <hr/>
+
+                {/* Movie Grid Cards */} 
+                <Row gutter={[16, 16]}>
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCards 
+                                image={movie.poster_path ? 
+                                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
+                
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button>Load More</button>
+            </div>
+
         </div>
     )
 }
 
-export default Auth(LandingPage, false)
+export default LandingPage
